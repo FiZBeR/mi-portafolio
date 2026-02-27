ed0e360c35725bbcd4228c30f2150c723cb0a0b1
CristianCV <cristiancv2000@hotmail.com>
2026-02-27 12:13:43 -0500

    feat: Detalles menores

--- src/main.js
+++ src/main.js
@@ -1,39 +1,42 @@
-const full = document.querySelector("html");
-const toggel_dark = document.querySelector('#toggel-dark');
-const formulario = document.querySelector('#contact-form');
-const mensaje = document.querySelector('#mensaje');
-const boton_cerrar = document.querySelector('#boton-close');
+document.addEventListener("DOMContentLoaded", () => {
+  const full = document.querySelector("html");
+  const toggel_dark = document.querySelector("#toggel-dark");
+  const formulario = document.querySelector("#contact-form");
+  const mensaje = document.querySelector("#mensaje");
+  const boton_cerrar = document.querySelector("#boton-close");
 
-toggel_dark.addEventListener('click', () => {
-  full.classList.toggle("dark")
-})
 
-formulario.addEventListener('submit', async (e) => {
-  e.preventDefault();
-  console.log('evento desactivado')
-
-  const formData = new FormData(formulario);
-
-  const response = await fetch(formulario.action, {
-    method: formulario.method,
-    body: formData,
-    headers: {
-      Accept: 'application/json',
-    },
+  toggel_dark.addEventListener("click", () => {
+    full.classList.toggle("dark");
   });
 
-  if(response.ok){
-    console.log("form enviado exitosamente");
-    mensaje.classList.remove('hidden');
-    formulario.reset();
+  formulario.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    console.log("evento desactivado");
 
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
+    const formData = new FormData(formulario);
+
+    const response = await fetch(formulario.action, {
+      method: formulario.method,
+      body: formData,
+      headers: {
+        Accept: "application/json",
+      },
     });
-  }
-});
 
-boton_cerrar.addEventListener('click', () => {
-  mensaje.classList.add('hidden');
-})
+    if (response.ok) {
+      console.log("form enviado exitosamente");
+      mensaje.classList.remove("hidden");
+      formulario.reset();
+
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+  });
+
+  boton_cerrar.addEventListener("click", () => {
+    mensaje.classList.add("hidden");
+  });
+});
---
+++ vite.config.js
@@ -0,0 +1,4 @@
+export default {
+    // Sustituye 'nombre-de-tu-repo' por el nombre exacto en GitHub
+    base: '/mi-portafolio/', 
+  }
